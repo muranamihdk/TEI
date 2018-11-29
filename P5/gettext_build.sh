646f6b788bb0d3e7dc65f21e67b9e02e8703eef4
Hideaki Muranami <muranamihideaki@gmail.com>
2018-11-29 18:05:06 +0900

    merge gettext_genmo.sh into gettext_build.sh

--- P5/gettext_build.sh
+++ P5/gettext_build.sh
@@ -29,6 +29,61 @@ do
   fi
 done
 
+
+echo "git checkout localize_ja"
+git checkout localize_ja
+echo
+
+if [ $(curl \
+ -s \
+ -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+ http://www3420ue.sakura.ne.jp:8080/api/projects/tei-guidelines-ja/repository/ \
+| jq -r '.needs_commit') = false ]
+then
+  echo "No changes to commit"
+  exit 0
+fi
+
+if [ $(curl \
+ -s \
+ -d operation=commit \
+ -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+ http://www3420ue.sakura.ne.jp:8080/api/components/tei-guidelines-ja/about/repository/ \
+| jq -r '.result') = false ]
+then
+  echo "Commit Failure."
+  exit 1
+else
+  echo "Commit Success."
+fi
+
+echo
+
+# poファイルをmoファイルに変換（Source/_mo 以下に poファイルと同じディレクトリ構成で）
+for SOURCE in `find "${PREFIX_PATH}/${PO_DIR}/${TARGET_LANG}/${GUIDELINES_DIR}/" -type f -name "*.po" -print`
+do
+  SOURCE_DIR=`dirname $SOURCE`
+  TARGET_DIR=${SOURCE_DIR/\/$PO_DIR/\/$MO_DIR}
+
+  if [ ! -d "$TARGET_DIR" ]
+  then
+    mkdir -p "$TARGET_DIR"
+    echo Created: "$TARGET_DIR"
+  fi
+
+  MO_FILE="$TARGET_DIR"/$(basename "$SOURCE" .po).mo
+  if [ ! -f "MO_FILE" ]
+  then
+    msgfmt "$SOURCE" -o "$MO_FILE"
+    echo Generated: "$MO_FILE"
+  elif [ "$SOURCE" -nt "$MO_FILE" ]
+  then
+    msgfmt "$SOURCE" -o "$MO_FILE"
+    echo Updated: "$MO_FILE"
+  fi
+done
+
+
 # mo ファイルから翻訳ファイルを作成（Source/Guidelines/ja 以下に）
 for SOURCE in `find "${PREFIX_PATH}/${MO_DIR}/${TARGET_LANG}/${GUIDELINES_DIR}/${TARGET_LANG}/" -type f -name "*.mo" -print`
 do
@@ -57,3 +112,31 @@ do
     echo Updated: "$TRANSLATED_FILE"
   fi
 done
+
+
+echo "git diff origin/localize_ja --name-only"
+git diff origin/localize_ja --name-only
+if [ $(git diff origin/localize_ja --name-only | wc -l) -ne 0 ]
+then
+  echo
+  git add .
+  git commit -m "update ja translation files"
+  echo
+  echo "git push origin localize_ja"
+  git push origin localize_ja
+fi
+
+# pull at Weblate
+if [ $(curl \
+ -s \
+ -d operation=pull \
+ -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
+ http://www3420ue.sakura.ne.jp:8080/api/components/tei-guidelines-ja/about/repository/ \
+| jq -r '.result') = false ]
+then
+  echo "Pull@Weblate Failure."
+  exit 1
+else
+  echo "Pull@Weblate Success."
+  echo
+fi
