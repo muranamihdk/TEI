c1a90db622f894d8edc265d366d6d8b46f581861
Hideaki Muranami <muranamihideaki@gmail.com>
2018-12-03 17:44:29 +0900

    modified gettext_update.sh pull_at_weblate.sh

--- P5/gettext_update.sh
+++ P5/gettext_update.sh
@@ -167,7 +167,7 @@ if [ $(curl \
  -s \
  -d operation=pull \
  -H "Authorization: Token 4uQDi24YSNEYgpxkkTvwdk7z9gZYupXiUvcxyccT" \
- http://www3420ue.sakura.ne.jp:8080/api/components/tei-guidelines-ja/about/repository/ \
+ http://www3420ue.sakura.ne.jp:8080/api/components/tei-guidelines-ja/ab-about/repository/ \
 | jq -r '.result') = false ]
 then
   echo "Pull at Weblate Failure."
