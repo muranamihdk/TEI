a233584a2eba7c8eaa3ad623ce99368661e22612
Hideaki Muranami <muranamihideaki@gmail.com>
2019-02-18 23:13:25 +0900

    modified build_guidelines_html_ja.sh

--- P5/build_guidelines_html_ja.sh
+++ P5/build_guidelines_html_ja.sh
@@ -1,14 +1,5 @@
 cd `dirname $0`
 make clean
 make html-web
-echo cp -r Guidelines-web/en ../../tei-guideline-ja/
-cp -r Guidelines-web/en ../../tei-guideline-ja/
 make clean
 make html-web ALLLANGUAGES=ja LANGUAGE=ja INPUTLANGUAGE=ja
-echo cp -r Guidelines-web/ja ../../tei-guideline-ja/
-cp -r Guidelines-web/ja ../../tei-guideline-ja/
-echo cd ../../tei-guideline-ja/
-cd ../../tei-guideline-ja/
-git add .
-git commit -m "rebuild html files"
-git push origin master
