7c5397c0504d61ec749b5c6afb2c121469a7185a
Hideaki Muranami <muranamihideaki@gmail.com>
2019-02-18 23:37:08 +0900

    modified build_guidelines_html_ja.sh, push_guidelines_html_ja.sh

--- P5/push_guidelines_html_ja.sh
+++ P5/push_guidelines_html_ja.sh
@@ -1,8 +1,4 @@
 cd `dirname $0`
-echo cp -r Guidelines-web/en ../../tei-guideline-ja/
-cp -r Guidelines-web/en ../../tei-guideline-ja/
-echo cp -r Guidelines-web/ja ../../tei-guideline-ja/
-cp -r Guidelines-web/ja ../../tei-guideline-ja/
 echo cd ../../tei-guideline-ja/
 cd ../../tei-guideline-ja/
 git add .
